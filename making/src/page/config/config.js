8acab9a45c7ea7562a030a07316c3a6b03690260
egookt3010 <egookt3010@gmail.com>
2022-11-30 16:25:33 +0700

    ego oktafanda

--- making/src/page/config/config.js
+++ making/src/page/config/config.js
@@ -1,8 +1,7 @@
 export const url_api = 'http://localhost/wizard-api/api/';
 export const url_assets = 'http://localhost/wizard-api/public/';
 export const url_base = 'http://localhost/wizard-api/';
-export const url_printing =
-  'http://prphoenix.gigades.id/Managements/suratPrint/';
+export const url_printing = 'https://v3.gigades.id/Managements/suratPrint/';
 
 // public server
 export const url_api_server = 'https://v3.gigades.id/rest/api/';
